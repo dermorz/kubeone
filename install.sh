3ef580081b4a5d9229cbb170ce56962c3fb694e3
Moritz Bracht <mail@morz.me>
2022-04-07 09:41:34 +0200

    Just a prank

--- install.sh
+++ install.sh
@@ -1,53 +1,7 @@
 #!/usr/bin/env sh
 
-# Copyright 2019 The KubeOne Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# This is a simple installer script for KubeOne.
-
-set -eu
-
-# What OS is used
-OS="$(uname | tr '[:upper:]' '[:lower:]')"
-# Find out what's the latest version
-VERSION="$(curl -w '%{url_effective}' -I -L -s -S https://github.com/kubermatic/kubeone/releases/latest -o /dev/null | sed -e 's|.*/v||')"
-# Download URL for the latest version
-URL="https://github.com/kubermatic/kubeone/releases/download/v${VERSION}/kubeone_${VERSION}_${OS}_amd64.zip"
-
-# 'kubeone' will be installed into this dir
-DEST=/usr/local/bin
-
-if [ ! -x "$(command -v unzip)" ]; then
-  echo "Your system is missing 'unzip'. Please install it and try again."
-  exit 2
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
 fi
-
-# Download the latest version for the OS and save it as zip
-if curl -LO "$URL"
-then
-  echo "Copying kubeone binary into $DEST"
-  
-  if unzip "kubeone_${VERSION}_${OS}_amd64.zip" -d "kubeone_${VERSION}_${OS}_amd64"
-  then
-    sudo mv "kubeone_${VERSION}_${OS}_amd64/kubeone" "$DEST"
-    rm "kubeone_${VERSION}_${OS}_amd64.zip"
-    echo "Kubermatic KubeOne has been installed into $DEST/kubeone"
-    echo "Terraform example configs, addons, and helper scripts have been downloaded into the ./kubeone_${VERSION}_${OS}_amd64 directory"
-    exit 0
-  fi
-else
-  printf "Failed to determine your platform.\n Try downloading from https://github.com/kubermatic/kubeone/releases"
-fi
-
-exit 1
+echo Good morning!
